89b2fd45c239ffc0ed916fe094150b4800f3904d
Prometeodev <prometeodev@users.noreply.github.com>
2026-03-29 09:56:03 +0200

    chore: add .gitignore for fuzz/target/ build artifacts

--- rust-core/novaterm-vt/fuzz/target/debug/deps/libfuzzer_sys-62f2368e8eda73bb.d
+++
@@ -1,5 +0,0 @@
-/data/data/com.termux/files/home/projects/novaterm/rust-core/novaterm-vt/fuzz/target/debug/deps/libfuzzer_sys-62f2368e8eda73bb.d: /data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libfuzzer-sys-0.4.12/src/lib.rs
-
-/data/data/com.termux/files/home/projects/novaterm/rust-core/novaterm-vt/fuzz/target/debug/deps/liblibfuzzer_sys-62f2368e8eda73bb.rmeta: /data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libfuzzer-sys-0.4.12/src/lib.rs
-
-/data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libfuzzer-sys-0.4.12/src/lib.rs:
--- rust-core/novaterm-vt/fuzz/target/debug/deps/rustix_openpty-5df4b36ed8054223.d
+++
@@ -1,5 +0,0 @@
-/data/data/com.termux/files/home/projects/novaterm/rust-core/novaterm-vt/fuzz/target/debug/deps/rustix_openpty-5df4b36ed8054223.d: /data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustix-openpty-0.2.0/src/lib.rs
-
-/data/data/com.termux/files/home/projects/novaterm/rust-core/novaterm-vt/fuzz/target/debug/deps/librustix_openpty-5df4b36ed8054223.rmeta: /data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustix-openpty-0.2.0/src/lib.rs
-
-/data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustix-openpty-0.2.0/src/lib.rs:
